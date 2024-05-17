2e166a16888c982e52c58ba393d4aecb9661bf64
thuan <thuanann.sama@gmail.com>
2024-05-17 21:30:44 +0700

    Revert "a"
    This reverts commit e32356b9b2109c16a70aa6ea5a82443a412c92bd.

--- MVC/Core/Database.php
+++ MVC/Core/Database.php
@@ -1,9 +1,9 @@
 <?php
-    class Database{
+    class Database extends PDO{
         const SERVER = 'localhost';
         const USER = 'root';
         const PASS = '';
-        const DB_NAME = 'fashionstore';
+        const DB_NAME = 'web2';
         public function connectDatabase()
         {
             $connect = mysqli_connect(self::SERVER, self::USER, self::PASS, self::DB_NAME);
